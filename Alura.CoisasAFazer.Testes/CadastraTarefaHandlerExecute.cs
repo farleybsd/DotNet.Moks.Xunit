20c262fb9c054c2de1e24e5e08dd830a5f722e65
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-04-16 01:25:41 -0300

    Setup da exceção

--- Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
+++ Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 using Xunit;
+using Moq;
 
 namespace Alura.CoisasAFazer.Testes
 {
@@ -40,11 +41,13 @@ namespace Alura.CoisasAFazer.Testes
             //arrange
             var comando = new CadastraTarefa("Estudar Xunit", new Categoria("Estudo"), new DateTime(2019, 12, 31));
             //var repo = new RepositorioFake(); //Dublê para o teste
-            var options = new DbContextOptionsBuilder<DbTarefasContext>()
-                                                                        .UseInMemoryDatabase("DbTarefasContext")
-                                                                        .Options;
-            var contexto = new DbTarefasContext(options);
-            var repo = new RepositorioTarefa(contexto);
+
+            var mock = new Mock<IRepositorioTarefas>(); 
+
+            mock.Setup(r => r.IncluirTarefas(It.IsAny<Tarefa[]>()))// parametro com os dados para o metodo IncluirTarefas
+                .Throws(new Exception("Houve Um Eroo na Inclusao de Tarefas"));
+
+            var repo = mock.Object;
             var handler = new CadastraTarefaHandler(repo);
 
             //act
